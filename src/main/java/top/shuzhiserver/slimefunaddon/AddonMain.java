d48aa512cf0d2c7d0b98adcc8bbfbab9cb63548a
shuz888 <103867824+shuz888@users.noreply.github.com>
2024-07-01 11:00:53 +0800

    增加吉利丁片

--- src/main/java/top/shuzhiserver/slimefunaddon/AddonMain.java
+++ src/main/java/top/shuzhiserver/slimefunaddon/AddonMain.java
@@ -27,11 +27,23 @@ public class AddonMain extends JavaPlugin implements SlimefunAddon {
         NamespacedKey categoryId = new NamespacedKey(this, "category1");
         CustomItemStack categoryItem = new CustomItemStack(Material.DIAMOND,"&4CAPTAINchad12自制扩展");
         ItemGroup itemGroup = new ItemGroup(categoryId,categoryItem);
-        SlimefunItemStack fishBoneItem = new SlimefunItemStack("FISH_BONE","eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYjQzMGFiNTQ1YjE0MjgyN2I5MzVmYzdlMzBmZDFhMTczZmE5MWI5M2NhYmJhNDVmODVmMjJhZWVkMGFlZmFjNyJ9fX0=","&5鱼骨","吉利丁片的原料","&r远古鱼类的化石,使用GEO矿机在河流,海洋群系挖掘");
+        SlimefunItemStack fishBoneItem = new SlimefunItemStack("FISH_BONE","eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYjQzMGFiNTQ1YjE0MjgyN2I5MzVmYzdlMzBmZDFhMTczZmE5MWI5M2NhYmJhNDVmODVmMjJhZWVkMGFlZmFjNyJ9fX0=","&5鱼骨","吉利丁片的原料");
         SlimefunItem fishBone = new SlimefunItem(itemGroup,fishBoneItem,RecipeType.GEO_MINER,new ItemStack[9]);
         fishBone.register(this);
         FishBoneResource resource = new FishBoneResource(this,fishBoneItem);
         resource.register();
+
+        SlimefunItemStack cowBoneItem = new SlimefunItemStack("COW_BONE","eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYjQzMGFiNTQ1YjE0MjgyN2I5MzVmYzdlMzBmZDFhMTczZmE5MWI5M2NhYmJhNDVmODVmMjJhZWVkMGFlZmFjNyJ9fX0=","&5牛骨","吉利丁片的原料");
+        ItemStack[] recipe = {
+                new ItemStack(Material.LEGACY_RAW_BEEF), null
+        };
+        SlimefunItem cowBone = new SlimefunItem(itemGroup,cowBoneItem,RecipeType.FOOD_FABRICATOR,recipe);
+        cowBone.register(this);
+        SlimefunItemStack gelatinSheetsItem = new SlimefunItemStack("GELATIN_SHEETS","eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvOGEyNGRjOWYxNzMzZjAwZTY3NjUwOWEyMjFiOGE2NmE2ZTA1OWU1NTA0NWQwOGE3ZTRmYzA4ZDdkMzEwODc1OCJ9fX0=","&5吉利丁片","可以打造各种软糖...");
+        recipe = new ItemStack[]{
+                fishBoneItem, cowBoneItem
+        };
+        SlimefunItem gelatinSheets = new SlimefunItem(itemGroup,gelatinSheetsItem,RecipeType.FOOD_FABRICATOR,recipe);
     }
 
     @Override
