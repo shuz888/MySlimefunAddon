27764a84696903c2baf2b5a2587d10821182c4c6
shuz888 <103867824+shuz888@users.noreply.github.com>
2024-07-02 08:02:01 +0800

    加入研究

--- src/main/java/top/shuzhiserver/slimefunaddon/AddonMain.java
+++ src/main/java/top/shuzhiserver/slimefunaddon/AddonMain.java
@@ -1,5 +1,6 @@
 package top.shuzhiserver.slimefunaddon;
 
+import io.github.thebusybiscuit.slimefun4.api.researches.Research;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
@@ -29,6 +30,8 @@ public class AddonMain extends JavaPlugin implements SlimefunAddon {
             // You could start an Auto-Updater for example
         }
 
+        NamespacedKey researchKey = new NamespacedKey(this, "ancient");
+        Research research = new Research(researchKey,1234012843,"成功解锁研究[远骨]",5);
         NamespacedKey categoryId = new NamespacedKey(this, "category1");
         CustomItemStack categoryItem = new CustomItemStack(Material.DIAMOND,"&4CAPTAINchad12自制扩展");
         ItemGroup itemGroup = new ItemGroup(categoryId,categoryItem);
@@ -52,6 +55,11 @@ public class AddonMain extends JavaPlugin implements SlimefunAddon {
                 cowBoneItem,  cowBoneItem, cowBoneItem ,
                 fishBoneItem, cowBoneItem, fishBoneItem
         };
+        research.addItems(fishBoneItem,cowBoneItem);
+        research.register();
+
+        researchKey = new NamespacedKey(this, "ancient");
+        research = new Research(researchKey, 1234012843, "成功解锁研究[软化剂]", 3);
         GelatinSheets gelatinSheets = new GelatinSheets(itemGroup,gelatinSheetsItem,RecipeType.ENHANCED_CRAFTING_TABLE,recipe);
         gelatinSheets.register(this);
         SlimefunItemStack diamondGummieItem = new SlimefunItemStack("DIAMOND_GUMMIE","eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvOGEyNGRjOWYxNzMzZjAwZTY3NjUwOWEyMjFiOGE2NmE2ZTA1OWU1NTA0NWQwOGE3ZTRmYzA4ZDdkMzEwODc1OCJ9fX0=","&5钻石软糖","入口即化,附带抗性提升5");
@@ -60,8 +68,14 @@ public class AddonMain extends JavaPlugin implements SlimefunAddon {
                 gelatinSheetsItem, new ItemStack(Material.DIAMOND), gelatinSheetsItem,
                 gelatinSheetsItem, new ItemStack(Material.SUGAR)  , gelatinSheetsItem
         };
+        research.addItems(gelatinSheetsItem);
+        research.register();
+
+        researchKey = new NamespacedKey(this, "ancient");
+        research = new Research(researchKey, 1234012843, "成功解锁研究[好吃的软糖]", 10);
         DiamondGummie diamondGummie = new DiamondGummie(itemGroup,diamondGummieItem,RecipeType.ENHANCED_CRAFTING_TABLE,recipe);
         diamondGummie.register(this);
+        research.addItems(diamondGummie);
         SlimefunItemStack emeraldGummieItem = new SlimefunItemStack("EMERALD_GUMMIE","eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvOTk4NWU5NjEyMjgzOTNhNGNkNTdmYWFlMTdiZmIwN2JkMGZkYTA4NTMxNmZkOGJlNTg0NTk4NzRkZWNjM2JjOCJ9fX0=","&5绿宝石软糖","入口即化,附带村庄英雄");
         recipe = new ItemStack[]{
                 gelatinSheetsItem, new ItemStack(Material.SUGAR)  , gelatinSheetsItem,
@@ -70,6 +84,7 @@ public class AddonMain extends JavaPlugin implements SlimefunAddon {
         };
         EmeraldGummie emeraldGummie = new EmeraldGummie(itemGroup,emeraldGummieItem,RecipeType.ENHANCED_CRAFTING_TABLE,recipe);
         emeraldGummie.register(this);
+        research.addItems(emeraldGummie);
         SlimefunItemStack waterGummieItem = new SlimefunItemStack("WATER_GUMMIE","eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvY2RkYWYyZTk0NDUzZjUzMDg2MWUwNWEwMGQ0MzRhZTRjMDFhYWZkMDQ5OWZmMmMyNGNiZTFiZjM0OTlmMmU0MCJ9fX0=","&5水软糖","入口即化,就是没啥味,附带水下呼吸");
         recipe = new ItemStack[]{
                 gelatinSheetsItem, gelatinSheetsItem  , gelatinSheetsItem,
@@ -77,7 +92,9 @@ public class AddonMain extends JavaPlugin implements SlimefunAddon {
                 gelatinSheetsItem, gelatinSheetsItem  , gelatinSheetsItem
         };
         WaterGummie waterGummie = new WaterGummie(itemGroup,waterGummieItem,RecipeType.ENHANCED_CRAFTING_TABLE,recipe);
-        emeraldGummie.register(this);
+        waterGummie.register(this);
+        research.addItems(waterGummie);
+        research.register();
     }
 
     @Override
